13eed41f27c964f5a057fb42afae9ab9501d64be
Alina Fitsner <code@getjoystick.com>
2023-10-20 13:39:01 +0300

    De lombok @Builder (remove Lombok @Builder annotations). This is first steps to de lombok all sdk code to improve compatibility with higher Java versions.

--- src/main/java/com/getjoystick/sdk/client/ClientConfig.java
+++ src/main/java/com/getjoystick/sdk/client/ClientConfig.java
@@ -3,98 +3,293 @@ package com.getjoystick.sdk.client;
 import com.getjoystick.sdk.cache.ApiCache;
 import com.getjoystick.sdk.cache.impl.ApiCacheLRU;
 import com.getjoystick.sdk.errors.ConfigurationException;
-import lombok.Builder;
-import lombok.Data;
-
 import java.util.Collections;
 import java.util.Map;
 import java.util.regex.Pattern;
 
-@Data
-@Builder(setterPrefix = "set")
 public class ClientConfig {
-
-    @Builder.Default
-    private String userId = "";
+    private String userId;
     private String apiKey;
     private String semVer;
-    @Builder.Default
-    private Map<Object, Object> params = Collections.emptyMap();
+    private Map<Object, Object> params;
     private int cacheTTL;
     private boolean serialized;
-    @Builder.Default
-    private ApiCache<String, String> cache = new ApiCacheLRU<>();
+    private ApiCache<String, String> cache;
 
-    /**
-     * Override standard builder with custom implementation
-     */
     public static ClientConfigBuilder builder() {
         return new OverridedClientConfigBuilder();
     }
 
-    /**
-     * Add additional validations as part of the build() method
-     */
-    private static class OverridedClientConfigBuilder extends ClientConfigBuilder {
+    private static Map<Object, Object> defaultParams() {
+        return Collections.emptyMap();
+    }
 
-        /**
-         * Semantic version validation from
-         * <a href="https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string">standard validation</a>
-         * without pre-release data.
-         */
+    private static ApiCache<String, String> defaultCache() {
+        return new ApiCacheLRU<>();
+    }
+
+    ClientConfig(String userId, String apiKey, String semVer, Map<Object, Object> params, int cacheTTL, boolean serialized, ApiCache<String, String> cache) {
+        this.userId = userId;
+        this.apiKey = apiKey;
+        this.semVer = semVer;
+        this.params = params;
+        this.cacheTTL = cacheTTL;
+        this.serialized = serialized;
+        this.cache = cache;
+    }
+
+    public String getUserId() {
+        return this.userId;
+    }
+
+    public String getApiKey() {
+        return this.apiKey;
+    }
+
+    public String getSemVer() {
+        return this.semVer;
+    }
+
+    public Map<Object, Object> getParams() {
+        return this.params;
+    }
+
+    public int getCacheTTL() {
+        return this.cacheTTL;
+    }
+
+    public boolean isSerialized() {
+        return this.serialized;
+    }
+
+    public ApiCache<String, String> getCache() {
+        return this.cache;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    public void setSemVer(String semVer) {
+        this.semVer = semVer;
+    }
+
+    public void setParams(Map<Object, Object> params) {
+        this.params = params;
+    }
+
+    public void setCacheTTL(int cacheTTL) {
+        this.cacheTTL = cacheTTL;
+    }
+
+    public void setSerialized(boolean serialized) {
+        this.serialized = serialized;
+    }
+
+    public void setCache(ApiCache<String, String> cache) {
+        this.cache = cache;
+    }
+
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        } else if (!(o instanceof ClientConfig)) {
+            return false;
+        } else {
+            ClientConfig other = (ClientConfig)o;
+            if (this.getCacheTTL() != other.getCacheTTL()) {
+                return false;
+            } else if (this.isSerialized() != other.isSerialized()) {
+                return false;
+            } else {
+                Object thisUserId = this.getUserId();
+                Object otherUserId = other.getUserId();
+                if (thisUserId == null) {
+                    if (otherUserId != null) {
+                        return  false;
+                    }
+                } else if (!thisUserId.equals(otherUserId)) {
+                    return false;
+                }
+
+                Object thisApiKey = this.getApiKey();
+                Object otherApiKey = other.getApiKey();
+                if (thisApiKey == null) {
+                    if (otherApiKey != null) {
+                        return false;
+                    }
+                } else if (!thisApiKey.equals(otherApiKey)) {
+                    return false;
+                }
+
+                Object thisSemVer = this.getSemVer();
+                Object otherSemVer = other.getSemVer();
+                if (thisSemVer == null) {
+                    if (otherSemVer != null) {
+                        return false;
+                    }
+                } else if (!thisSemVer.equals(otherSemVer)) {
+                    return false;
+                }
+
+                Object thisParams = this.getParams();
+                Object otherParams = other.getParams();
+                if (thisParams == null) {
+                    if (otherParams != null) {
+                        return false;
+                    }
+                } else if (!thisParams.equals(otherParams)) {
+                    return false;
+                }
+
+                Object thisCache = this.getCache();
+                Object otherCache = other.getCache();
+                if (thisCache == null) {
+                    return otherCache == null;
+                } else return thisCache.equals(otherCache);
+            }
+        }
+    }
+
+    public int hashCode() {
+        int result = 1;
+        result = result * 59 + this.getCacheTTL();
+        result = result * 59 + (this.isSerialized() ? 79 : 97);
+        Object userIdObj = this.getUserId();
+        result = result * 59 + (userIdObj == null ? 43 : userIdObj.hashCode());
+        Object apiKeyObj = this.getApiKey();
+        result = result * 59 + (apiKeyObj == null ? 43 : apiKeyObj.hashCode());
+        Object semVerObj = this.getSemVer();
+        result = result * 59 + (semVerObj == null ? 43 : semVerObj.hashCode());
+        Object paramsObj = this.getParams();
+        result = result * 59 + (paramsObj == null ? 43 : paramsObj.hashCode());
+        Object cacheObj = this.getCache();
+        result = result * 59 + (cacheObj == null ? 43 : cacheObj.hashCode());
+        return result;
+    }
+
+    public String toString() {
+        return "ClientConfig(userId=" + this.getUserId() + ", apiKey=" + this.getApiKey() + ", semVer=" + this.getSemVer() + ", params=" + this.getParams() + ", cacheTTL=" + this.getCacheTTL() + ", serialized=" + this.isSerialized() + ", cache=" + this.getCache() + ")";
+    }
+
+    public static class ClientConfigBuilder {
+        private boolean userIdSet;
+        private String userIdValue;
+        private String apiKey;
+        private String semVer;
+        private boolean paramsSet;
+        private Map<Object, Object> paramsValue;
+        private int cacheTTL;
+        private boolean serialized;
+        private boolean cacheSet;
+        private ApiCache<String, String> cacheValue;
+
+        ClientConfigBuilder() {
+        }
+
+        public ClientConfigBuilder setUserId(String userId) {
+            this.userIdValue = userId;
+            this.userIdSet = true;
+            return this;
+        }
+
+        public ClientConfigBuilder setApiKey(String apiKey) {
+            this.apiKey = apiKey;
+            return this;
+        }
+
+        public ClientConfigBuilder setSemVer(String semVer) {
+            this.semVer = semVer;
+            return this;
+        }
+
+        public ClientConfigBuilder setParams(Map<Object, Object> params) {
+            this.paramsValue = params;
+            this.paramsSet = true;
+            return this;
+        }
+
+        public ClientConfigBuilder setCacheTTL(int cacheTTL) {
+            this.cacheTTL = cacheTTL;
+            return this;
+        }
+
+        public ClientConfigBuilder setSerialized(boolean serialized) {
+            this.serialized = serialized;
+            return this;
+        }
+
+        public ClientConfigBuilder setCache(ApiCache<String, String> cache) {
+            this.cacheValue = cache;
+            this.cacheSet = true;
+            return this;
+        }
+
+        public ClientConfig build() {
+            String thisUserIdValue = this.userIdValue;
+            if (!this.userIdSet) {
+                thisUserIdValue = "";
+            }
+
+            Map<Object, Object> thisParamsValue = this.paramsValue;
+            if (!this.paramsSet) {
+                thisParamsValue = ClientConfig.defaultParams();
+            }
+
+            ApiCache<String, String> thisCacheValue = this.cacheValue;
+            if (!this.cacheSet) {
+                thisCacheValue = ClientConfig.defaultCache();
+            }
+
+            return new ClientConfig(thisUserIdValue, this.apiKey, this.semVer, thisParamsValue, this.cacheTTL, this.serialized, thisCacheValue);
+        }
+
+        public String toString() {
+            return "ClientConfig.ClientConfigBuilder(userId$value=" + this.userIdValue + ", apiKey=" + this.apiKey + ", semVer=" + this.semVer + ", params$value=" + this.paramsValue + ", cacheTTL=" + this.cacheTTL + ", serialized=" + this.serialized + ", cache$value=" + this.cacheValue + ")";
+        }
+    }
+
+    private static class OverridedClientConfigBuilder extends ClientConfigBuilder {
         private static final String SEM_VER_REGEXP = "^(0|[1-9]\\d*)\\.(0|[1-9]\\d*)\\.(0|[1-9]\\d*)$";
         private static final Pattern SEM_VER_PATTERN = Pattern.compile(SEM_VER_REGEXP);
 
-        /**
-         * Fast-fail semantic version validation
-         *
-         * @throws ConfigurationException in case of invalid semantic version
-         */
-        private void validateSemVersion(final String version) {
+        private OverridedClientConfigBuilder() {
+        }
+
+        private void validateSemVersion(String version) {
             if (version != null && !SEM_VER_PATTERN.matcher(version).matches()) {
                 throw new ConfigurationException("The semantic version [" + version + "] is incorrect.");
             }
         }
 
-        /**
-         * Fast-fail cache expiration value validation
-         *
-         * @throws ConfigurationException in case of invalid value
-         */
-        private void validateCacheTTL(final int seconds) {
+        private void validateCacheTTL(int seconds) {
             if (seconds < 0) {
                 throw new ConfigurationException("Cache expiration time must be defined as seconds and must be positive.");
             }
         }
 
-        /**
-         * @param semVer is a semantic version of your app that is making the request. Format example: '1.0.0'
-         * @return this {@link  ClientConfigBuilder} instance
-         */
         @Override
-        public ClientConfigBuilder setSemVer(final String semVer) {
-            validateSemVersion(semVer);
+        public ClientConfigBuilder setSemVer(String semVer) {
+            this.validateSemVersion(semVer);
             return super.setSemVer(semVer);
         }
 
-        /**
-         * @param cacheTTL is an expiration time in seconds to invalidate the cache
-         * @return this {@link  ClientConfigBuilder} instance
-         */
         @Override
-        public ClientConfigBuilder setCacheTTL(final int cacheTTL) {
-            validateCacheTTL(cacheTTL);
+        public ClientConfigBuilder setCacheTTL(int cacheTTL) {
+            this.validateCacheTTL(cacheTTL);
             return super.setCacheTTL(cacheTTL);
         }
-
         @Override
         public ClientConfig build() {
-            if (super.apiKey == null || super.apiKey.trim().isEmpty()) {
+            if (super.apiKey != null && !super.apiKey.trim().isEmpty()) {
+                return super.build();
+            } else {
                 throw new ConfigurationException("API key is not provided.");
             }
-            return super.build();
         }
-
     }
-
 }
--- src/main/java/com/getjoystick/sdk/models/PublishData.java
+++ src/main/java/com/getjoystick/sdk/models/PublishData.java
@@ -2,51 +2,156 @@ package com.getjoystick.sdk.models;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.getjoystick.sdk.errors.ConfigurationException;
-import lombok.Builder;
-import lombok.Data;
+import java.util.Arrays;
 
-@Data
-@Builder(setterPrefix = "set")
-@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonInclude(Include.NON_NULL)
 public class PublishData {
-
-    /**
-     * Description of the content
-     */
-    @JsonProperty(value = "d")
+    @JsonProperty("d")
     String description;
-    /**
-     * Content which can be encoded to JSON (including strings, numbers, boolean, maps and arrays)
-     */
-    @JsonProperty(value = "c")
+    @JsonProperty("c")
     Object content;
+    @JsonProperty("m")
+    Object[] dynamicContentMap;
 
-    @Builder.Default
-    @JsonProperty(value = "m")
-    Object[] dynamicContentMap = new Object[0];
-
-    public static PublishData.PublishDataBuilder builder() {
-        return new PublishData.OverridePublishDataBuilder();
+    private static Object[] defaultDynamicContentMap() {
+        return new Object[0];
     }
 
-    private static class OverridePublishDataBuilder extends
-        PublishData.PublishDataBuilder {
-        @Override
+    PublishData(String description, Object content, Object[] dynamicContentMap) {
+        this.description = description;
+        this.content = content;
+        this.dynamicContentMap = dynamicContentMap;
+    }
+
+    public static PublishDataBuilder builder() {
+        return new PublishDataBuilder();
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public Object getContent() {
+        return this.content;
+    }
+
+    public Object[] getDynamicContentMap() {
+        return this.dynamicContentMap;
+    }
+
+    @JsonProperty("d")
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @JsonProperty("c")
+    public void setContent(Object content) {
+        this.content = content;
+    }
+
+    @JsonProperty("m")
+    public void setDynamicContentMap(Object[] dynamicContentMap) {
+        this.dynamicContentMap = dynamicContentMap;
+    }
+
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        } else if (!(o instanceof PublishData)) {
+            return false;
+        } else {
+            PublishData other = (PublishData)o;
+            Object thisDescription = this.getDescription();
+            Object otherDescription = other.getDescription();
+            if (thisDescription == null) {
+                if (otherDescription != null) {
+                    return false;
+                }
+            } else if (!thisDescription.equals(otherDescription)) {
+                return false;
+            }
+
+            Object thisContent = this.getContent();
+            Object otherContent = other.getContent();
+            if (thisContent == null) {
+                if (otherContent != null) {
+                    return false;
+                }
+            } else if (!thisContent.equals(otherContent)) {
+                return false;
+            }
+
+            return Arrays.deepEquals(this.getDynamicContentMap(), other.getDynamicContentMap());
+        }
+    }
+
+    public int hashCode() {
+        int result = 1;
+        Object descriptionObj = this.getDescription();
+        result = result * 59 + (descriptionObj == null ? 43 : descriptionObj.hashCode());
+        Object contentObj = this.getContent();
+        result = result * 59 + (contentObj == null ? 43 : contentObj.hashCode());
+        result = result * 59 + Arrays.deepHashCode(this.getDynamicContentMap());
+        return result;
+    }
+
+    public String toString() {
+        return "PublishData(description=" + this.getDescription() + ", content=" + this.getContent() + ", dynamicContentMap=" + Arrays.deepToString(this.getDynamicContentMap()) + ")";
+    }
+
+    public static class PublishDataBuilder {
+        private String description;
+        private Object content;
+        private boolean dynamicContentMapSet;
+        private Object[] dynamicContentMapValue;
+
+        PublishDataBuilder() {
+        }
+
+        @JsonProperty("d")
+        public PublishDataBuilder setDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        @JsonProperty("c")
+        public PublishDataBuilder setContent(Object content) {
+            this.content = content;
+            return this;
+        }
+
+        @JsonProperty("m")
+        public PublishDataBuilder setDynamicContentMap(Object[] dynamicContentMap) {
+            this.dynamicContentMapValue = dynamicContentMap;
+            this.dynamicContentMapSet = true;
+            return this;
+        }
+
         public PublishData build() {
-            if (super.description == null) {
+            if (description == null) {
                 throw new ConfigurationException("Description is not provided.");
+            } else {
+                int length = description.trim().length();
+                if (length >= 1 && length <= 50) {
+                    if (content == null) {
+                        throw new ConfigurationException("Content is not provided.");
+                    }
+                } else {
+                    throw new ConfigurationException("There must be more than 0 and no more than 50 symbols in the description.");
+                }
             }
-            final int length = super.description.trim().length();
-            if (length < 1 || length > 50) {
-                throw new ConfigurationException(
-                    "There must be more than 0 and no more than 50 symbols in the description."
-                );
+            Object[] thisDynamicContentMapValue = this.dynamicContentMapValue;
+            if (!this.dynamicContentMapSet) {
+                thisDynamicContentMapValue = PublishData.defaultDynamicContentMap();
             }
-            if (super.content == null) {
-                throw new ConfigurationException("Content is not provided.");
-            }
-            return super.build();
+
+            return new PublishData(this.description, this.content, thisDynamicContentMapValue);
+        }
+
+        public String toString() {
+            return "PublishData.PublishDataBuilder(description=" + this.description + ", content=" + this.content + ", dynamicContentMap$value=" + Arrays.deepToString(this.dynamicContentMapValue) + ")";
         }
     }
 }
